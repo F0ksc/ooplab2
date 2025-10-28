1961dfd1cadd8bee3d21c92a0860e2f77ecd9558
Nikolay <capusta3333@gmail.com>
2025-10-28 12:08:46 +0200

    Try numba 2 to push h file

--- HAIRDRESSER.h
+++ HAIRDRESSER.h
@@ -5,19 +5,16 @@
 #include <ctime>
 #include <cstdlib>
 #include <algorithm>
+#include "Person.h"
 
-using namespace std;
-
-class customer { // Пункт 1 // Пункт 13
+class customer : public Person { // Пункт 1 // Пункт 13 // (3)Пункт 1
 private: // Пункт 2 // Пункт 13
-	string name;
-	long long phone;
 	int membershipId;
 	int spent;
 	int membershipPoints;
 	static int totalCustomers;	// Пункт (2)4
 public:
-	string id;
+	std::string id;
 	bool membership;
 	bool elder;
 
@@ -26,20 +23,25 @@ public:
 		
 	customer();	// Пункт (2) 1
 
-	customer(string name, long long phone, int membershipId, string id, bool membership, bool elder);	// Пункт (2)1
+	customer(std::string name, long long phone, int membershipId, std::string id, bool membership, bool elder);	// Пункт (2)1 // (3)Пункт 5
 
 	customer(const customer& other);	// Пункт (2)1
 
 	void updatePhonenumber(int newPhone);
-	void addSpent(int amount, bool elder);
 
-	void addSpent(int amount, bool elder, bool usingPoints);
+	std::string getId() const;
+
+	void printDetails();
+
+	void addSpent(double amount, bool elder);
+
+	void addSpent(double amount, bool elder, bool usingPoints);
 
 	void eldereligible(int age);
 
 	void merge(customer dublicate);
 
-	customer referalLink(string name, string id, double phone);
+	customer referalLink(std::string name,long long phone, std::string id);
 
 	// Пункт 5 // Пункт 13
 
@@ -53,21 +55,20 @@ public:
 	~customer();	// Пункт (2)3
 };
 
-class hairDresser { // Пункт 1 // Пункт 13
+class hairDresser : public Person { // Пункт 1 // Пункт 13
 private: // Пункт 2 // Пункт 13
-	string employeeId;
+	std::string employeeId;
 	customer currentClient; // Пункт 3 // Пункт 13 // Пункт (2) 13
 	int exp;
 	int hourRate;
 	bool canColor;
 public:
-	string name;
 	int workstation;
 	bool available;
 
 	// Пункт 4 // Пункт 13
 
-	void creatingHD(string id, int exp, int hourRate, bool canColor, string name, int workstation);
+	hairDresser(std::string name, long long phone, std::string id, int exp, int hourRate, bool canColor, int workstation);
 
 	void printSchedule();
 
@@ -92,7 +93,7 @@ public:
 
 // Пункт (2) 12 ->
 
-class Barbershop {
+class Barbershop : public Person{
 public:
 	hairDresser* staff[5];
 	int numStaff;
@@ -109,3 +110,23 @@ public:
 };
 
 //  <- Пункт (2) 12
+
+class ClientRecord : private customer { // (3)Пункт 3
+public:
+	using customer::id; // (3)Пункт 6
+	using customer::addSpent; // (3)Пункт 6
+	using customer::printDetails; // (3)Пункт 6
+
+	ClientRecord(std::string name, long long phone, int mID, std::string id);
+};
+
+class Accountant {
+public:
+	void calculateSalary();
+};
+
+class Manager : public hairDresser, public Accountant { // (3)Пункт 4
+public:
+	Manager(std::string name, long long phone, std::string empId, int hourRate);
+
+};
